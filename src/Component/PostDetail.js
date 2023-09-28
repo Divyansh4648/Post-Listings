f67c402bb63bb06e8fd27216cd2d8bb27879ec8a
Divyansh4648 <63147400+Divyansh4648@users.noreply.github.com>
2023-09-29 03:55:33 +0530

    Update PostDetail.js

--- src/Component/PostDetail.js
+++ src/Component/PostDetail.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import './PostDetails.css'; // Import your CSS file
+import './PostDetails.css'; 
 
 const PostDetail = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
-    // Fetch the details of the selected post
     const fetchData = async () => {
       const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
       const data = await response.json();
